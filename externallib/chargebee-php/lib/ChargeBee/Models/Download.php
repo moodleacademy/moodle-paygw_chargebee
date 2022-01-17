0b373534eeba5c75c305c962041aff2c360eab5f
Rajneel Totaram <rjnlfj@yahoo.com>
2022-01-18 10:55:26 +1200

    Add chargebee-php library

---
+++ externallib/chargebee-php/lib/ChargeBee/Models/Download.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ChargeBee\ChargeBee\Models;
+
+use ChargeBee\ChargeBee\Model;
+use ChargeBee\ChargeBee\Request;
+use ChargeBee\ChargeBee\Util;
+
+class Download extends Model
+{
+
+  protected $allowed = [
+    'downloadUrl',
+    'validTill',
+  ];
+
+
+
+  # OPERATIONS
+  #-----------
+
+ }
+
+?>
---
+++ externallib/chargebee-php/lib/ChargeBee/Models/ExportDownload.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ChargeBee\ChargeBee\Models;
+
+use ChargeBee\ChargeBee\Model;
+
+class ExportDownload extends Model
+{
+  protected $allowed = [
+    'downloadUrl',
+    'validTill',
+  ];
+
+}
+
+?>
