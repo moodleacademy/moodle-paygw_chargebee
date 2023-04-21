4999a58ebf63f2fa4048364d607c7e7a8fd35c9a
Rajneel Totaram <rjnlfj@yahoo.com>
2023-04-21 14:45:11 +1200

    Bump version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'paygw_chargebee';
 $plugin->release = '0.1.0';
-$plugin->version = 2022041907;
+$plugin->version = 2023042000;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
